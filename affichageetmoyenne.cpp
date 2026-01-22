77c9d1ba58ee11df6007ec9bf9ae103726b72f75
yassine <yassine.imejjane@lyceejulesfil.fr>
2026-01-22 16:05:10 +0100

    new file:   affichageetmoyenne.cpp

---
+++ affichageetmoyenne.cpp
@@ -0,0 +1,8 @@
+#include <iostream>  // inclut les entrées/sorties standard
+
+using namespace std; // pour éviter d'écrire std::cout à chaque fois
+
+int main() {
+    cout << "Hello World" << endl; // affiche le message
+    return 0;                      // indique que le programme s'est terminé correctement
+}
