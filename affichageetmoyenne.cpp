ba652432d6e9c4dbe2fd24a3b2638d1d1069834b
matheocazabant <matheo.cazabant@lyceejulesfil.fr>
2026-01-22 16:59:22 +0100

    modified:   affichageetmoyenne.cpp

--- affichageetmoyenne.cpp
+++ affichageetmoyenne.cpp
@@ -1,8 +1,18 @@
-#include <iostream>  // inclut les entrées/sorties standard
+#include <iostream>
+#include <ctime>   // Pour time()
+#include <cstdlib> // Pour rand() et srand()
 
-using namespace std; // pour éviter d'écrire std::cout à chaque fois
+using namespace std;
 
 int main() {
-    cout << "Hello World" << endl; // affiche le message
-    return 0;                      // indique que le programme s'est terminé correctement
+    // Initialisation du générateur (à ne faire qu'une seule fois au début du main)
+    srand(time(0)); 
+
+    // Générer une note aléatoire entre 0 et 20
+    // On utilise le modulo % 21 car rand() % 21 donne un reste entre 0 et 20
+    float note_aleatoire = rand() % 21;
+
+    cout << "Note generee : " << note_aleatoire << "/20" << endl;
+
+    return 0;
 }
