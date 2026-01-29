45f5f59ae951d00e2269beaf8ca9d9b73e8b54cf
daviddupsg <david.lacomme@lyceejulesfil.fr>
2026-01-29 15:11:51 +0100

    Changes to be committed:

--- rechercheetbest.cpp
+++ rechercheetbest.cpp
@@ -1,9 +1,43 @@
 #include <iostream>
+#include "inspecteur.h"
 
-int main() 
-{
-    // Affiche "Hello World!" dans la console
-    std::cout << "Hello World!" << std::endl;
+using namespace std;
 
-    return 0;
+// Recherche d'un étudiant par son nom
+void rechercherEtudiant(Etudiant classe[], int nbEtudiants, const string& nom) {
+    bool trouve = false;
+
+    for (int i = 0; i < nbEtudiants; i++) {
+        if (classe[i].nom == nom) {
+            cout << "Etudiant trouve :" << endl;
+            cout << "Nom  : " << classe[i].nom << endl;
+            cout << "Note : " << classe[i].note << endl;
+            trouve = true;
+            break;
+        }
+    }
+
+    if (!trouve) {
+        cout << "Etudiant non trouve." << endl;
+    }
+}
+
+// Recherche de la meilleure note
+void meilleureNote(Etudiant classe[], int nbEtudiants) {
+    if (nbEtudiants == 0) {
+        cout << "Aucun etudiant dans la classe." << endl;
+        return;
+    }
+
+    int indexMax = 0;
+
+    for (int i = 1; i < nbEtudiants; i++) {
+        if (classe[i].note > classe[indexMax].note) {
+            indexMax = i;
+        }
+    }
+
+    cout << "Meilleure note de la classe :" << endl;
+    cout << "Nom  : " << classe[indexMax].nom << endl;
+    cout << "Note : " << classe[indexMax].note << endl;
 }
