13f5f7ded5fe66a9f65c0863755cd61bbc15e89c
cyril <cyril.chevalier@lyceejulesfil.fr>
2026-01-29 14:58:46 +0100

    Changes to be committed:

--- menubloucle.cpp
+++ menubloucle.cpp
@@ -1,9 +1,43 @@
 #include <iostream>
+#include "menuboucle.h"
 
-int main() 
-{
-    // Affiche "Hello World!" dans la console
-    std::cout << "Hello World!" << std::endl;
+using namespace std;
 
-    return 0;
+void ajouterEleve(Etudiant classe[], int &nbElv) {
+    if (nbElv < 30) {
+        cout << "Nom de l'eleve : ";
+        cin >> classe[nbElv].nom;
+        cout << "Note de l'eleve : ";
+        cin >> classe[nbElv].note;
+        nbElv++;
+    } else {
+        cout << "Classe pleine !" << endl;
+    }
+}
+
+void afficherEleves(Etudiant classe[], int nbElv) {
+    cout << "--- Liste des eleves ---" << endl;
+    for (int i = 0; i < nbElv; i++) {
+        cout << classe[i].nom << " : " << classe[i].note << "/20" << endl;
+    }
+}
+
+void calculerMoyenne(Etudiant classe[], int nbElv) {
+    if (nbElv == 0) return;
+    float somme = 0;
+    for (int i = 0; i < nbElv; i++) {
+        somme += classe[i].note;
+    }
+    cout << "Moyenne de la classe : " << somme / nbElv << endl;
+}
+
+void meilleureNote(Etudiant classe[], int nbElv) {
+    if (nbElv == 0) return;
+    int indexMax = 0;
+    for (int i = 1; i < nbElv; i++) {
+        if (classe[i].note > classe[indexMax].note) {
+            indexMax = i;
+        }
+    }
+    cout << "Meilleur eleve : " << classe[indexMax].nom << " avec " << classe[indexMax].note << "/20" << endl;
 }
---
+++ menuboucle.h
@@ -0,0 +1,13 @@
+#ifndef MENU_BOUCLE_H
+#define MENU_BOUCLE_H
+
+#include <string>
+
+
+// Déclarations des fonctions
+void ajouterEleve(Etudiant classe[], int &nbElv);
+void afficherEleves(Etudiant classe[], int nbElv);
+void calculerMoyenne(Etudiant classe[], int nbElv);
+void meilleureNote(Etudiant classe[], int nbElv);
+
+#endif
